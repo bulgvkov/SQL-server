0c7115b6ee2540c7765c1ac70ceae8784bfa79f7
bulgvkov <45395246+bulgvkov@users.noreply.github.com>
2021-03-22 13:26:27 +0300

    Триггер для удаления сотрудника из таблицы "Сотрудник"

--- triggerForDeleteEmployee.sql
+++ triggerForDeleteEmployee.sql
@@ -1,13 +1,15 @@
-﻿USE [HR_dep]
+USE [HR_dep]
 GO
-/****** Object:  Trigger [dbo].[delete_empl]    Script Date: 20.03.2021 21:04:24 ******/
+/****** Object:  Trigger [dbo].[delete_empl]    Script Date: 22.03.2021 13:04:37 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 ALTER trigger [dbo].[delete_empl]
 on [dbo].[Сотрудник]
-after delete
+instead of delete
 as
 delete from Документ_сотрудника
-where ID_сотрудника_FK = (select ID_сотрудника from deleted) 
+where ID_сотрудника_FK = (select ID_сотрудника from deleted)
+delete from Сотрудник
+where ID_сотрудника = (select ID_сотрудника from deleted)
